6ea03ba77f3fa744c1b0e19160ba1ae11f1293bb
Ray Fix <rayfix@gmail.com>
2014-06-22 00:04:37 -0700

    Initial Commit

---
+++ RAFIntMath/RAFIntMath.h
@@ -0,0 +1,19 @@
+//
+//  RAFIntMath.h
+//  RAFIntMath
+//
+//  Created by Ray Fix on 6/22/14.
+//  Copyright (c) 2014 Pelfunc, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RAFIntMath.
+FOUNDATION_EXPORT double RAFIntMathVersionNumber;
+
+//! Project version string for RAFIntMath.
+FOUNDATION_EXPORT const unsigned char RAFIntMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RAFIntMath/PublicHeader.h>
+
+
